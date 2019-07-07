ee752607fe1fa7c6dfb952c63fad1746e68d3dce
drinkcoffee <dcoffie92@gmail.com>
2019-07-06 21:16:00 -0700

    added a few better functions to perform checks. added a button for music. added some poke styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,6 +18,7 @@ for(let i = 0; i < computerChoice.length; i++) {
 
 console.log(nameToGuess);
 
+//funciton to create a number underscores based on the lenght of the word to guess
 function makeUnderscores() {
     for (let i = 0; i < nameToGuess.length; i++){
         let dashContiner = document.createElement("span");
@@ -40,12 +41,12 @@ $(document).ready(function() {
 });
 
 
-
+//game variables 
 let usersGuesses = [];
 let guessesLeft = nameToGuess.length;
 let numberOfWins = 0;
 let numberOfLosses = 0;
-let correctGuess = 0;
+
 
 
 //check if user typed a letter 
@@ -53,7 +54,7 @@ let correctGuess = 0;
 function lettersOnly() {
     var charCode = event.keyCode;
 
-    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || charCode == 8){
+    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123)){
         return true;
     }
         else{
@@ -61,12 +62,18 @@ function lettersOnly() {
         }
 };
 
+
 document.onkeyup = function(event){
         //text displaying game info
         let winDisplay = document.getElementById("wins");
         let lossDisplay = document.getElementById("loss");
         let guessDisplay = document.getElementById("current-guesses");
         let guessNumber = document.getElementById("guesses-left");
+        //display number of guesses 
+        guessNumber.textContent = "Guesses Left: " + guessesLeft;
+        let youLose = document.getElementById("lostText");
+        youLose.textContent = "";
+        //checks to see if guess is in the array of the word to guess
         let guessIndex = nameToGuess.indexOf(event.key);
            //function that resets the game
         function resetGame(){
@@ -82,44 +89,95 @@ document.onkeyup = function(event){
             guessNumber.textContent = "Guesses Left: " + guessesLeft;
             correctGuess = 0;
         };
+        //gets rid of the previous answer 
         function deleteName() {
             for (let i = 0; i < nameToGuess.length; i++){
             let spanToRemove = document.getElementById("pokeDash-"+i);
                 spanToRemove.parentNode.removeChild(spanToRemove);
             }
         }
+        //find the duplicates in an array and store the index of each
+        function getDuplicates(array1, value){
+            let dups = [];
+            for(let i = 0; i<array1.length;i++){
+                if(array1[i] === value){
+                    dups.push(i);
+                    }
+                }
+            return dups;
+        };
+        
+        //replace the values in target array with 0s using integer values from a passed in array
+        function replace(anArray1,anArray2) {
+            
+            for (var i = 0; i < anArray1.length; i++){
+                anArray2.splice(anArray1[i],1,0);
+            }
+            return anArray2;
+        };
+
+        //updates all the underscore which match the guessed letter
+        function updateLetters(foundArray){
+            for (let i = 0; i < foundArray.length;i++){
+                let updateLetter = document.getElementById("pokeDash-"+foundArray[i]);
+                 updateLetter.textContent = event.key;
+            }
+        };
+
+        function checkWin(value){
+            return value === 0;
+        };
+        console.log(nameToGuess);
+
 
   //only run the game if a letter was guessed
   if(lettersOnly()){
-      if(guessesLeft > 0){
+    console.log(guessesLeft);
+        //if the user still have guess left,play game
+      if(guessesLeft > 1){
+
              //store users guess into an array
             usersGuesses.push(event.key);
             //update display with letter guesses 
             guessDisplay.textContent = "Your guesses so far: " + usersGuesses;
-            //tick down guesses left
-            
-            //update display with number of guesses left 
-             guessNumber.textContent = "Guesses Left: " + guessesLeft;
 
+            //if guess exsists, will be a positive number 
              if(guessIndex > -1){
-                 let updateLetter = document.getElementById("pokeDash-"+guessIndex);
-                 updateLetter.textContent = event.key;
-                 correctGuess++;
-                //  console.log(nameToGuess);
-                 if (correctGuess === nameToGuess.length){
+ 
+                 //reveal the guessed letters
+                 updateLetters(getDuplicates(nameToGuess,event.key));
+                //find the duplicates (if any) and make an array of its indexs
+                //then use that array to replace the values with 0 so we don't need to find them again
+                replace(getDuplicates(nameToGuess, event.key),nameToGuess);
+                console.log(nameToGuess);
+
+                // when the user has as many correct guesses as the length of the the word to guess, end the game 
+                 if (nameToGuess.every(checkWin)){
                      numberOfWins++;
                      winDisplay.textContent = "Wins: " + numberOfWins;
+                     youLose.textContent = "Good Job! The pokemon was " + computerChoice;
                      deleteName();
                      resetGame();
                      makeUnderscores();
-                     console.log(nameToGuess);
+                    //  console.log(nameToGuess);
                  }
              }
              else if(guessIndex === -1){
+                // console.log(guessIndex);
                 guessesLeft--;
                 guessNumber.textContent = "Guesses Left: " + guessesLeft;
+               
+                
              }
              
+      }
+      else{
+        numberOfLosses++;
+        lossDisplay.textContent = "Losses: " + numberOfLosses;
+        youLose.textContent = "Sorry. The pokemon was " + computerChoice;
+        deleteName();
+        resetGame();
+        makeUnderscores();
       }
   }
 
